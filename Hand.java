414349d73a5f0c0df99d92067568258f1a7d7d1c
Steve Aronson <saronson@lfschools.net>
2021-01-26 08:57:18 -0600

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -13,19 +13,5 @@ public class Hand {
    public String toString() {
    }
    
-   /**
-    * returns the blackjack value of the hand
-    * Each card is worth its face value
-    * with the following exceptions:
-    *
-    * Jack, Queen and King are worth 10 each.
-    *
-    * The first Ace is usually worth 11.
-    * Every other Ace is worth 1.
-    * However, if the hand is a bust (over 21),
-    * then the first Ace should be 1, too.
-    */
-   public int getBlackjackValue() {
-      return 0;
-   }
+  
 }
