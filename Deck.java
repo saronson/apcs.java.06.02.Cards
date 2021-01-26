ccb22d3246549d21ab3db21cf8d2afcba311500b
Steve Aronson <saronson@lfschools.net>
2021-01-26 09:24:38 -0600

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -8,32 +8,24 @@
 public class Deck
 {
 
-   public Deck() {
-   }
-   
-   /** 
-    * return a String of all existing cards in the deck
-    * HINT:  Call the toString method for each card
-    */
-   public String toString() {
-       return "";
-   }
-   
-   /**
-    * return true if the deck has no cards left,
-    * otherwise return false
-    */
-   public boolean isEmpty() {
-       return true;
-   }
-   
-   /**
-    * return the number of cards left in the deck
-    */
-   public int numCards() {
-       return 0;
-   }
-    
+    public Deck() {
+    }
+
+    /**
+     * return true if the deck has no cards left,
+     * otherwise return false
+     */
+    public boolean isEmpty() {
+        return true;
+    }
+
+    /**
+     * return the number of cards left in the deck
+     */
+    public int numCards() {
+        return 0;
+    }
+
     /**
      * deal a card from the "top" of the deck
      * (you decide where the top is...)
@@ -42,17 +34,36 @@ public class Deck
     public Card dealCard() {
         return null;
     }
-    
+
     /**
      * shuffle the deck so that all cards still left
      * are rearranged in some random order
      */
     public void shuffle() {
     }
-    
+
     /**
      *  add the given card back into the deck
      */
     public void addCard(Card card) {
+    }
+
+    /** 
+     * return a String of all existing cards in the deck
+     * HINT:  Call the toString method for each card
+     */
+    public String toString() {
+        return "";
+    }
+
+    /**
+     * Returns true if all the cards are the same as another 
+     * deck in the same position.
+     */
+    public boolean equals(Object o)
+    {
+        Deck other = (Deck)o;
+
+        return false;
     }
 }
