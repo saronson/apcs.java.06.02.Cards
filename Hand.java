a0b1d2ee81c06e84bdae3b03fc8300fb1de0ae72
Steve Aronson <saronson@lfschools.net>
2021-01-26 09:26:25 -0600

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -3,36 +3,37 @@
  */
 public class Hand {
 
-   /**
-    * creates a hand of the given number of cards
-    * dealt from the given deck
-    */
-   public Hand(Deck deck, int numCards) {
-   }
-   
-   /**
-    * return a String of all the cards in the hand
-    */
-   public String toString() {
-   }
-   
-   /**
-    * Put each card back into the deck 
-    * by calling the deck's addCard method.
-    */
+    /**
+     * creates a hand of the given number of cards
+     * dealt from the given deck
+     */
+    public Hand(Deck deck, int numCards) {
+    }
+
+    /**
+     * return a String of all the cards in the hand
+     */
+    public String toString() {
+        return "";
+    }
+
+    /**
+     * Put each card back into the deck 
+     * by calling the deck's addCard method.
+     */
     public void putCardsBack(Deck deck)
     {
     }
-   
+
     /**
      * Returns true if all the cards are the same as another 
      * hand in the same position.
      */
     public boolean equals(Object o)
     {
-       Hand other = (Hand)o;
-       
-       return false;
+        Hand other = (Hand)o;
+
+        return false;
     }
 
 }
