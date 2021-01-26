bda3d26dba4a9da9d68469057f671581e4f72b83
Steve Aronson <saronson@lfschools.net>
2021-01-26 08:51:11 -0600

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,3 +1,13 @@
+/**
+ * Create a well-designed Card class with proper:
+ * - instance variables
+ * - constants
+ * - accessors
+ * - mutators
+ * - constructors 
+ * - toString
+ * - equals
+ */
 public class Card
 {
   
