069c2627bb77231feb1b9f0c40158c028747ddae
Steve Aronson <saronson@lfschools.net>
2021-01-26 09:01:03 -0600

    Update and rename DogArrayTest.java to Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+
+
+/**
+ * Describe your class here!
+ */
+public class Main 
+{
+  
+    public static void main(String args[])
+    {
+       Main main = new Main();
+       main.play();
+    }
+    
+  
+    public void play()
+    {
+       
+    }
+  
+
+}
