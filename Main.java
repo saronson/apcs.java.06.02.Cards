0c9a049d4fa5e99591a8f9d164bb60e35b65b0d6
Steve Aronson <saronson@lfschools.net>
2022-01-17 14:02:44 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@
  */
 public class Main 
 {
+    private BlackjackHand[] hands;
+
     public static void main(String args[])
     {
         Main main = new Main();
@@ -15,14 +17,23 @@ public class Main
      * Write the game of blackjack for the 
      *    given number of players.
      * 1) create a deck and shuffle it
-     * 2) create an array of BlackjackHand objects 
+     * 2) fill the hands array with the properly created 
+     *    number of BlackjackHand objects
      * 3) print the hands and their values by calling 
     *     toString and getBlackjackValue for each hand
-     * 4) print who won.  
+     * 4) print who won. If a tie, print all who tied
+     * 5) return the index of the hand that won, -1 if a tie  
      */
-    public void playBlackjack(int numPlayers)
+    public int playBlackjack(int numPlayers)
     {
+       return -1;
+    }
 
+    /**
+     * DO NOT USE THIS OR MODIFY THIS
+     */
+    public BlackjackHand[] getBlackjackHands() {
+      return hands;
     }
 
 }
