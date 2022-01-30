2b28625e73de967d96faaea76e01f9a11ebb7ccb
Steve Aronson <saronson@lfschools.net>
2022-01-30 08:03:33 -0600

    added testGame

--- DeckTest.java
+++ DeckTest.java
@@ -9,7 +9,6 @@ import java.util.*;
 public class DeckTest extends junit.framework.TestCase
 {
 
-    private  boolean failed = false;
     private  Class<?> c;
     private Constructor constructor;
 
@@ -196,7 +195,7 @@ public class DeckTest extends junit.framework.TestCase
                     sum += (int)num;
 
                 }
-      
+
                 if (sum == 370 || sum == 360) {
                     foundAceAce = true;
                 } else if (sum != 380) {
@@ -216,6 +215,57 @@ public class DeckTest extends junit.framework.TestCase
         }
 
     }
+
+    @Test
+    public void testGame() {
+
+        try
+        {
+            /*
+            Class<?> main = Class.forName("Main");
+            Constructor cMain = main.getConstructor();
+            Object game = cMain.newInstance();
+            Method m = main.getDeclaredMethod("playBlackjack");
+             */
+            Main game = new Main();
+            for (int numPlayers = 2; numPlayers < 10; numPlayers++) {
+                int[] winners = new int[numPlayers+1];
+                for (int k = 0; k < 1000; k++) {
+                    //       Object result = m.invoke(game, j);
+                    int result = game.playBlackjack(numPlayers);
+                    BlackjackHand[] hands = game.getBlackjackHands();
+                    assertTrue("should create hands", hands != null);
+                    assertEquals("Should have " + numPlayers + " players", numPlayers, hands.length);
+                    int winner = 0;
+
+                    for (int i = 1; i < hands.length; i++) {
+                        assertTrue("hand should not be null", hands[i] != null);
+                        if (hands[i].getBlackjackValue() > hands[winner].getBlackjackValue())
+                            winner = i;
+                        else if (hands[i].getBlackjackValue() == hands[winner].getBlackjackValue()) {
+                            winner = -1;
+                            break;
+                        }
+                    }
+                    assertEquals("wrong winner/tie", winner, result);
+                    if (winner == -1)
+                      winner = hands.length;
+                    winners[winner] += 1;
+                }
+                for (int i = 0; i < winners.length; i++) {
+                    assertTrue("imbalance in winners, was deck shuffled?", winners[i]>0);
+                }
+
+
+                //      System.out.println(result);
+            }
+
+        }catch (Exception e) {
+            failure(e.toString());
+        }
+
+    }
+
     private void failure(String str)
     {
         //    System.out.println("*** Failed: " + str);
