93c1354f55d42ab1229c1e7f718e31fb4ca9d3ca
Steve Aronson <saronson@lfschools.net>
2022-01-29 19:56:34 -0600

    Update DeckTest.java

--- DeckTest.java
+++ DeckTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 import java.lang.reflect.*;
+import java.util.*;
 /**
  * This class tests the Deck class
  * 
@@ -15,7 +16,7 @@ public class DeckTest extends junit.framework.TestCase
     public static void main(String[] args)
     {
         DeckTest ct = new DeckTest ();
-        ct.test();
+        ct.testDeck();
     }
 
     public void checkField(Object t1, String fieldType, String fieldName, Object value, boolean isFinal) {
@@ -48,7 +49,7 @@ public class DeckTest extends junit.framework.TestCase
     }
 
     @Test
-    public void test() {
+    public void testDeck() {
         String className = "Deck";
         Object t1 = null;
         try
@@ -79,30 +80,59 @@ public class DeckTest extends junit.framework.TestCase
                 failure("numCards not working properly");
             if (!callGet(t1, "isEmpty").equals(false))
                 failure("isEmpty not working properly");  
-            System.out.println(t1);
+            HashMap<String, Integer> map = new HashMap<String, Integer>();
+
             for (int i = 51; i >= 0; i--) {
                 Object obj = callGet(t1, "dealCard");
                 tempCards[51-i] = obj;
 
                 if (!callGet(t1, "numCards").equals(i))
-                    failure("dealCard and/or numCards not working properly");   
+                    failure("dealCard and/or numCards not working properly"); 
+                String str = obj.toString();
+                if (map.get(str) != null)
+                    System.out.println(str + " already exists");
+                else
+                    map.put(str, 1);
+
             }
+            if (!callGet(t1, "numCards").equals(0))
+                failure("dealCard and/or numCards not working properly"); 
+
             if (!callGet(t1, "isEmpty").equals(true))
-                failure("dealCard and/or isEmpty not working properly"); 
+                failure("isEmpty not working properly"); 
             for (int i = 0; i < tempCards.length; i++) {
                 callSet(t1, "addCard", Card.class, tempCards[i]);
                 if (!callGet(t1, "isEmpty").equals(false))
-                    failure("addCard and/or isEmpty not working properly"); 
+                    failure("isEmpty not working properly"); 
 
                 if (!callGet(t1, "numCards").equals(i+1))
-                    failure("addCard and/or numCards not working properly");   
+                    failure("dealCard and/or numCards not working properly");   
             }
+            t1 = constructor.newInstance();
+
             String answer = "Ace of Spades, Ace of Hearts, Ace of Diamonds, Ace of Clubs, 2 of Spades, 2 of Hearts, 2 of Diamonds, 2 of Clubs, 3 of Spades, 3 of Hearts, 3 of Diamonds, 3 of Clubs, 4 of Spades, 4 of Hearts, 4 of Diamonds, 4 of Clubs, 5 of Spades, 5 of Hearts, 5 of Diamonds, 5 of Clubs, 6 of Spades, 6 of Hearts, 6 of Diamonds, 6 of Clubs, 7 of Spades, 7 of Hearts, 7 of Diamonds, 7 of Clubs, 8 of Spades, 8 of Hearts, 8 of Diamonds, 8 of Clubs, 9 of Spades, 9 of Hearts, 9 of Diamonds, 9 of Clubs, 10 of Spades, 10 of Hearts, 10 of Diamonds, 10 of Clubs, Jack of Spades, Jack of Hearts, Jack of Diamonds, Jack of Clubs, Queen of Spades, Queen of Hearts, Queen of Diamonds, Queen of Clubs, King of Spades, King of Hearts, King of Diamonds, King of Clubs";
             Object str = callGet(t1, "toString");
-            System.out.println(str);
+
             if (!str.equals(answer)) {
                 failure("toString is getting\n   " + str + "\n but should be\n   " + answer);
             }
+            Method m = c.getDeclaredMethod("shuffle");
+            m.invoke(t1);
+            for (int i = 0; i < 52; i++) {
+                Object obj = callGet(t1, "dealCard");
+
+                if (!callGet(t1, "numCards").equals(51-i))
+                    failure("shuffle not working properly"); 
+                String str2 = obj.toString();
+                if (map.get(str2) == null)
+                    failure(str2 + " does not exist (or is there multiple times) after shuffle");
+                else
+                    map.remove(str2);
+
+            }
+            if (map.size() != 0)
+                failure("Not all cards in deck after shuffle.");
+
         }catch (Exception e) {
             failure(e.toString());
         }
