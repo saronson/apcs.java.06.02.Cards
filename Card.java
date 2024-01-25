8c3c6f1a55d2369e8f8c6ebb80994108f2b9a074
Steve Aronson <saronson@lfschools.net>
2024-01-25 11:54:43 -0600

    Update Card.java

--- Card.java
+++ Card.java
@@ -3,29 +3,38 @@
  * - int instance variables for suit and rank
  *     Suits: Spades (1), Hearts (2), Diamonds (3), Clubs (4)
  *     Ranks: Ace (1), 2-10, Jack (11), Queen (12), King (13)
- * - constants
+ * - constants (especially for the special values described right above here)
  *   i.e. there should be no "magic numbers" in your code
  * - constructor that takes suit and rank parameters (in that order)
- * - accessors getSuit and getRank
- * - mutators setSuit and setRank
+ * - accessors (getters) getSuit and getRank
+ * - mutators (setters) setSuit and setRank
  * - public String getSuitString()
  *     return a String for the suit like Spades, Hearts, etc.
  * - public String getRankString()
- *     return a String for the rank like 4, 6, Jack, Queen, Ace, etc.
+ *     return a String for the rank like 4, 6, Jack, Queen, King, Ace, etc.
  * - public String toString()
  *     formatted exactly like these examples: 
  *     "9 of Spades", "Ace of Hearts", "King of Clubs"
- * - public equals(Object other)
+ * - public equals(Object other) (finish the given method)
  *     true if two cards have the same suits and ranks
  */
 public class Card
 {
+    // add instance variables here
+
+    // add suit constants here
+    public static final int SPADES = 1; // this is an example constant for the suits
+
+    // add rank constants here
+    public static final int JACK = 11; // this is an example constant for the ranks
+
 
    /**
     * return true if this.suit and this.rank are the same as other.suit and other.rank respectively, otherwise return false.
     */
    public boolean equals(Object obj) {
       Card other = (Card)obj;
+      
       return false;
    }
 }
